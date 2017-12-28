45cd4c23eecf4b53a343d467e028e05060c17648
Feilding Cage <feilding.cage@gmail.com>
2017-12-28 11:35:45 +0000

    updates to remove y and replace w top

--- src/js/app.js
+++ src/js/app.js
@@ -11,35 +11,38 @@ var importMap = new Promise(function(resolve, reject) {
 
 importMap.then(function(){
 
-var tractive = document.querySelector('.interactive');
-var fallerPaths = [].slice.apply(document.querySelectorAll('path:not([data-band = "0"])'));
-var fallerPathsLessCountries = fallerPaths.filter(function(p) {
-    return p.classList.contains('gv-band-no-data') == false;
+  var mapTransitioned = false;
+  var tractive = document.querySelector('.interactive');
+  var fallerPaths = [].slice.apply(document.querySelectorAll('path:not([data-band = "0"])'));
+  var fallerPathsLessCountries = fallerPaths.filter(function(p) {
+      return p.classList.contains('gv-band-no-data') == false;
+  })
+  var heading = document.querySelector('.gv-heading')
+  var caption1 = document.querySelector('.gv-caption#one')
+
+
+  tractive.addEventListener("click",function(e){
+      fallerPathsLessCountries.map(function(p){
+          p.classList.toggle("hidden");
+      })
+  })
+
+  function removeFallers() {
+      mapTransitioned = true;
+      fallerPathsLessCountries.map(function(p){
+          p.classList.add("hidden");
+      })
+      caption1.classList.add("visible")
+  }
+
+  window.addEventListener("scroll", function(e){
+      if(!mapTransitioned){
+        console.log('klsjdflkjsdf')
+        var hbcr = mapboard.getBoundingClientRect();
+        if (hbcr.top < 100 && hbcr.top > 0 ) {
+          removeFallers();
+        }
+      }
+  })
+
 })
-var heading = document.querySelector('.gv-heading')
-var caption1 = document.querySelector('.gv-caption#one')
-
-
-tractive.addEventListener("click",function(e){
-    fallerPathsLessCountries.map(function(p){
-        p.classList.toggle("hidden");
-    })
-})
-
-function removeFallers() {
-    fallerPathsLessCountries.map(function(p){
-        p.classList.add("hidden");
-    })
-    caption1.classList.add("visible")
-}
-
-window.addEventListener("scroll", function(e){
-    var hbcr = mapboard.getBoundingClientRect();
-    if (hbcr.y < 100 && hbcr.y > 0 ) {
-      removeFallers();
-    }
-})
-
-})
-
-
